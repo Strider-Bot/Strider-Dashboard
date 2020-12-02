f05aa7d4d0e218165cf4d505ccba7afc7b9bba54
Mental <73140396+MaximKing1@users.noreply.github.com>
2020-12-02 14:23:07 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 {
-  "token": "",
+  "token": "NzY1MDg4OTA4NzczODE4Mzc4.X4Pukg.3UsqIvJh5DGxAbpRMIGwaoUD9Wc",
   "prefix": "$",
   "mongo": "mongodb+srv://StriderUser:nypO3E7cnpzP8jO8@cluster0.z38we.mongodb.net/strider-bannedwords?retryWrites=true&w=majority"
 }
